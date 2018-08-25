8f71e29871e9ac066387d2c574f019334bd4aa2c
Angad Nadkarni <angad@hullo.ai>
2018-08-25 18:18:04 +0530

    Added godoc for Tabular struct

--- tabular.go
+++ tabular.go
@@ -5,17 +5,21 @@ import (
 	"strings"
 )
 
+// Tabular is a representation fo a Database Table, with it's Name and columns listed in
+// a serial order.
 type Tabular struct {
 	Name   string
 	Fields []string
 }
 
+// New constructs a Tabular given a table-name and column-fields.
 func New(name string, fields ...string) (tabular Tabular) {
 	tabular.Name = name
 	tabular.Fields = fields
 	return
 }
 
+// Insertion generates an "INSERT INTO [Name] (...) VALUES (?...)" query.
 func (tabular Tabular) Insertion(queryFmt string) (query string) {
 	return fmt.Sprintf(queryFmt, fmt.Sprintf(
 		"INSERT INTO `%s` (%s) VALUES (%s)",
@@ -25,6 +29,11 @@ func (tabular Tabular) Insertion(queryFmt string) (query string) {
 	))
 }
 
+// Selection generates a list of all field-names including the joined ones in serial
+// order. It is a fragment of SQL, and is hence unexecutable by itself. It needs to be
+// prefixed with a "SELECT " and suffixed with a " FROM...", that includes and defines
+// the specific JOIN relationships. The fragment is incomplete by design, as it allows
+// the user to define virtual fields, etc. in the query if required.
 func (tabular Tabular) Selection(queryFmt string, joined ...Tabular) (query string) {
 	q := func(t Tabular) string {
 		return fmt.Sprintf("`%s`.`%s`", t.Name, strings.Join(
